96b1e6f9d79b7bc92361c2b21d86f485e8584368
Deathics <timur_kn08@mail.ru>
2022-06-07 15:46:00 +0400

    Добавьте файлы проекта.

---
+++ ConsoleGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleGame.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Map map = new Map();
+            string newMap = string.Join(null, map.GameMap);
+            map.DrawMap(newMap);
+        }
+    }
+}
